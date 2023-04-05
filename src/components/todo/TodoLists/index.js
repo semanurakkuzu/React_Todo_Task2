5d47b1c175c122cbd1b844c12d08a0a655844a5d
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-05 14:39:07 +0300

    added delete function

--- src/components/todo/TodoLists/index.js
+++ src/components/todo/TodoLists/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 
-function TodoLists({todos}) {
+function TodoLists({todos, setTodos}) {
     
+    function deleteTodo(index) {
+        setTodos(todos.filter((_, todoIndex) => todoIndex !== index));
+      }
+
   return (
     <section className="main">
           <input className="toggle-all" type="checkbox" />
           <label htmlFor="toggle-all">Mark all as complete</label>
 
           <ul className="todo-list">
-            {todos && todos.map((todo, i) =>
-            <li className="completed" key={i}>
+            {todos && todos.map((todo, index) =>
+            <li className="completed" key={index}>
               <div className="view">
                 <input className="toggle" type="checkbox" />
                 <label>{todo}</label>
-                <button className="destroy"></button>
+                <button className="destroy" onClick={() => deleteTodo(index)}></button>
               </div>
             </li>
            )}
