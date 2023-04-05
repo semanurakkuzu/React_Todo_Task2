efdce5f6b7daa5089d45012b323a753a32aefc73
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-05 20:05:23 +0300

    added filter component

---
+++ src/components/todo/Filter/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+function Filter({
+  todos,
+  setTodos,
+  setFilteredTodos,
+  filterType,
+  setFilterType,
+  filteredTypeTodos
+}) {
+  function changeFilter(filter) {
+    setFilterType(filter)
+    setFilteredTodos(filteredTypeTodos(filter))
+  }
+
+  function clearCompleted() {
+    setTodos(todos.filter((item) => item.completed === false))
+  }
+
+  return (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong>{todos.filter((item) => !item.completed).length}</strong> items
+        left
+      </span>
+      <ul className="filters">
+        <li>
+          <a
+            href="#/"
+            className={filterType === 'all' ? 'selected' : ''}
+            onClick={() => changeFilter('all')}
+          >
+            All
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/"
+            className={filterType === 'active' ? 'selected' : ''}
+            onClick={() => changeFilter('active')}
+          >
+            Active
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/"
+            className={filterType === 'completed' ? 'selected' : ''}
+            onClick={() => changeFilter('completed')}
+          >
+            Completed
+          </a>
+        </li>
+      </ul>
+      <button className="clear-completed" onClick={ clearCompleted}>Clear completed</button>
+    </footer>
+  )
+}
+
+export default Filter
