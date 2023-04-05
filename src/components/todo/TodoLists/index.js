0739e9baa197f21297a21cb45626d87b0729e71e
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-05 16:02:07 +0300

    added checktodo function

--- src/components/todo/TodoLists/index.js
+++ src/components/todo/TodoLists/index.js
@@ -1,29 +1,47 @@
 import React from 'react'
 
-function TodoLists({todos, setTodos}) {
-    
-    function deleteTodo(index) {
-        setTodos(todos.filter((_, todoIndex) => todoIndex !== index));
-      }
+function TodoLists({ todos, setTodos }) {
+  function deleteTodo(index) {
+    setTodos(todos.filter((_, todoIndex) => todoIndex !== index))
+  }
+
+  function checkTodo(index) {
+    setTodos(
+      todos.map((todo, todoIndex) => {
+        if (todoIndex !== index) {
+          return todo
+        }
+        todo.completed = !todo.completed
+        return todo
+      })
+    )
+  }
 
   return (
     <section className="main">
-          <input className="toggle-all" type="checkbox" />
-          <label htmlFor="toggle-all">Mark all as complete</label>
+      <input className="toggle-all" type="checkbox" />
+      <label htmlFor="toggle-all">Mark all as complete</label>
 
-          <ul className="todo-list">
-            {todos && todos.map((todo, index) =>
-            <li className="completed" key={index}>
+      <ul className="todo-list">
+        {todos &&
+          todos.map((todo, index) => (
+            <li key={index} className={todo.completed ? 'completed' : ''}>
               <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>{todo}</label>
-                <button className="destroy" onClick={() => deleteTodo(index)}></button>
+                <input
+                  className="toggle"
+                  type="checkbox"
+                  onClick={() => checkTodo(index)}
+                />
+                <label>{todo.text}</label>
+                <button
+                  className="destroy"
+                  onClick={() => deleteTodo(index)}
+                ></button>
               </div>
             </li>
-           )}
-         
-          </ul>
-        </section>
+          ))}
+      </ul>
+    </section>
   )
 }
 
