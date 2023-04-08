c2062aacf24b164792750272d0da911e464621bb
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-08 15:09:09 +0300

    added editing functions

--- src/components/todo/TodoLists/index.js
+++ src/components/todo/TodoLists/index.js
@@ -1,38 +1,64 @@
+
 import React, { useState } from 'react'
 
 function TodoLists({ todos, setTodos, filteredTodos }) {
   const [allCompleted, setAllCompleted] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState('');
 
-  function deleteTodo(id) {
-    setTodos(todos.filter((todo) => todo.id !== id))
-  }
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-  function checkTodo(id) {
+  const checkTodo = (id) => {
     setTodos(
       todos.map((todo) => {
         if (todo.id !== id) {
-          return todo
+          return todo;
         }
 
-        todo.completed = !todo.completed
-
-        return todo
+        return { ...todo, completed: !todo.completed };
       })
-    )
-  }
+    );
+  };
 
-  function toggleAll() {
-    setTodos(todos.map(todo => ({
-      ...todo,
-      completed: !allCompleted
-    })));
+  const toggleAll = () => {
+    setTodos(todos.map((todo) => ({ ...todo, completed: !allCompleted })));
     setAllCompleted(!allCompleted);
-  }
+  };
+
+  const startEditing = (id, text) => {
+    setIsEditing(id);
+    setEditedText(text);
+  };
+
+
+  const saveEditing = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, text: editedText };
+        }
+
+        return todo;
+      })
+    );
+
+    setIsEditing(false);
+    setEditedText('');
+  };
 
   return (
     <section className="main">
-      <input className="toggle-all" type="checkbox" checked={allCompleted} onChange={toggleAll} />
-      <label htmlFor="toggle-all" onClick={toggleAll}>Mark all as complete</label>
+      <input
+        className="toggle-all"
+        type="checkbox"
+        checked={allCompleted}
+        onChange={toggleAll}
+      />
+      <label htmlFor="toggle-all" onClick={toggleAll}>
+        Mark all as complete
+      </label>
 
       <ul className="todo-list">
         {filteredTodos &&
@@ -45,17 +71,37 @@ function TodoLists({ todos, setTodos, filteredTodos }) {
                   checked={todo.completed}
                   onChange={() => checkTodo(todo.id)}
                 />
-                <label>{todo.text}</label>
-                <button
-                  className="destroy"
-                  onClick={() => deleteTodo(todo.id)}
-                ></button>
+                {isEditing === todo.id ? (
+                  <>
+                    <input
+                      className='inputEdit'
+                      type="text"
+                      value={editedText}
+                      onChange={(e) => setEditedText(e.target.value)}
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                          saveEditing(todo.id);
+                        } 
+                        
+                      }}
+                      onClick={() => saveEditing(todo.id)}
+                      autoFocus
+                    /> 
+                  </>
+                ) : (
+                  <>
+                    <label onClick={() => startEditing(todo.id, todo.text)}>
+                      {todo.text}
+                    </label>
+                    <button  className="destroy" onClick={() => deleteTodo(todo.id)}></button>
+                  </>
+                )}
               </div>
             </li>
           ))}
       </ul>
     </section>
-  )
+  );
 }
 
-export default TodoLists
+export default TodoLists;
